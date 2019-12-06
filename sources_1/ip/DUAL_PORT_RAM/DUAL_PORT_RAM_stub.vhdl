a92faac3c260ad4da056bc5df56ff02fe4b62471
Robin <robinnollet@gmail.com>
2019-12-06 10:57:17 +0100

    error uit memory_if gehaald

--- sources_1/ip/DUAL_PORT_RAM/DUAL_PORT_RAM_stub.vhdl
+++ sources_1/ip/DUAL_PORT_RAM/DUAL_PORT_RAM_stub.vhdl
@@ -1,10 +1,10 @@
 -- Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
 -- --------------------------------------------------------------------------------
 -- Tool Version: Vivado v.2019.2 (win64) Build 2708876 Wed Nov  6 21:40:23 MST 2019
--- Date        : Thu Dec  5 10:10:30 2019
--- Host        : DESKTOP-VSQA5BK running 64-bit major release  (build 9200)
--- Command     : write_vhdl -force -mode synth_stub
---               C:/VHDL_Spectrogram/VHDL_Spectrogram.srcs/sources_1/ip/DUAL_PORT_RAM/DUAL_PORT_RAM_stub.vhdl
+-- Date        : Fri Dec  6 10:51:57 2019
+-- Host        : LAPTOP-69E4OMV9 running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub {D:/Ing/Master
+--               1sem/Digitale/VHDL_Spectrogram/VHDL_Spectrogram.srcs/sources_1/ip/DUAL_PORT_RAM/DUAL_PORT_RAM_stub.vhdl}
 -- Design      : DUAL_PORT_RAM
 -- Purpose     : Stub declaration of top-level module interface
 -- Device      : xc7z020clg484-1
